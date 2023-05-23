5321d30c29c448159d136911d5ead73324f120cf
AmandaVongayi <86626516+AmandaVongayi@users.noreply.github.com>
2023-05-23 14:55:47 +0200

    .

--- main.js
+++ main.js
@@ -16,96 +16,6 @@ $(document).ready(function() {
 
 			// end hamburger menu
 
-// Reveal and hidee header
-
-// const header = document.querySelector("header");
-// const logo = document.getElementsByClassName("logo-div");
-// let lastScroll = 0;
-
-// const throttle = (func, time = 100) => {
-//   let lastTime = 0;
-//   return () => {
-//     const now = new Date();
-//     if (now - lastTime >= time) {
-//       func();
-//       time = now;
-//     }
-//   };
-// };
-
-// const validateHeader = () => {
-//   const windowY = window.scrollY;
-//   const windowH = window.innerHeight;
-//   if (windowY > windowH) {
-//     // We passed the first section, set a toggable class
-//     header.classList.add("is-fixed");
-//     // logo.classList.add("show")
-//     // Determine is we ready to animate
-//     if (windowY > windowH + 5) {
-//       header.classList.add("can-animate");
-//       if (windowY < lastScroll) {
-//         // Determine if we scrolling up
-//         header.classList.add("scroll-up");
-//       } else {
-//         header.classList.remove("scroll-up");
-//       }
-//     } else {
-//       header.classList.remove("scroll-up");
-//     }
-//   } else {
-//     header.classList.remove("is-fixed", "can-animate");
-//   }
-//   lastScroll = windowY;
-// };
-
-// window.addEventListener("scroll", throttle(validateHeader, 40));
-
-
-// New scroll function
-// Hide Header on on scroll down
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $('header').outerHeight();
-
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
-
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//     }
-// }, 250);
-
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
-    
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
-    
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (st > lastScrollTop && st > navbarHeight){
-//         // Scroll Down
-//         $('header').removeClass('nav-down').addClass('nav-up');
-//     } else {
-//         // Scroll Up
-//         if(st + $(window).height() < $(document).height()) {
-//             $('header').removeClass('nav-up').addClass('nav-down');
-//         }
-//     }
-    
-//     lastScrollTop = st;
-// }
-
-
-
-
-
-
 //   Slick Slider Elements
 
 	$('.single-item').slick(
@@ -142,78 +52,27 @@ $(document).ready(function() {
 	
 				window.addEventListener("scroll", reveal);
 
-
-// New New Hide Show Header 
-
-// var hideHeader =$(".header-to-hide");
-// // var showHeader = $('.header-to-show');
-// var pos =  hideHeader.postion();
-
-// $(window).scroll(function() {
-// 		var windowpos = $(window).scrollTop();
-// 		if (windowpos >= pos.top & windowpos >=20) {
-// 			hideHeader.addClass("hide");
-// 		} else {
-// 			hideHeader.removeClass("hide");	
-// 		} });
-
-
-// When the user scrolls the page, execute myFunction
-
-// Fetch the header to hide on scroll and assign
-// var hideHeader = document.getElementById("hideHeader");
-// // Fetch the header to show on scroll up and assign
-// var showHeader = document.getElementById("showHeader");
-// // Set Last Scroll:
-// // var lastScroll = 0;
-
-// // Position of red header
-// var pos = hideHeader.position();
-// $(window).scroll(function() {
-// 	// var st = $(window).scrollTop();
-// 	var windowpos = $(window).scrollTop();
-// 	if (windowpos >= pos.top & windoepos >=20){
-// 		// downscroll code
-// 		hideHeader.addClass("hideHeader");
-// 	} else {
-// 		// upscroll code
-// 		hideHeader.removeClass("hideHeader");
-// 	}
-// 	lastScroll = st;
-//  });
-
-
-//  var hideLogo = $(".logo");
-// 	var pos = hideLogo.position();					   
-// 	$(window).scroll(function() {
-// 		var windowpos = $(window).scrollTop();
-// 		if (windowpos >= pos.top & windowpos >=20) {
-// 			hideLogo.addClass("hide");
-// 		} else {
-// 			hideLogo.removeClass("hide");	
-// 		}
-// 	});
-
-			
-// End of JS
-
-
+// Header Styles
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 var hideHeader = document.getElementById("hideHeader");
+var logo = document.getElementById("logo");
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   // Scroll Down
   if (prevScrollpos > currentScrollPos) {
     hideHeader.style.top = "0";
-    hideHeader.classList.remove("bg-new");
-    hideHeader.classList.add("bg-initial");
+
   } 
   // Scroll Up
   else {
+    // hideHeader.removeClass("bg-initial");
+    // hideHeader.addClass("bg-new");
     hideHeader.style.top = "-190px";
-    hideHeader.classList.remove("bg-initial");
-    hideHeader.classList.add("bg-new");
+	hideHeader.classList.remove("headerBackgroundInitial");
+	hideHeader.classList.add("headerBackgroundNew");
+	logo.classList.remove("hide");
+	logo.classList.add("logo-show")
   }
   prevScrollpos = currentScrollPos;
 };
